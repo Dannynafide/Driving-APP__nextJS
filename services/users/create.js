7ef122c1bcf886f129020f7bc3de544a39968d74
Daniel Pakoca <danielpakoca@gmail.com>
2023-07-04 09:21:39 +0200

    User registration -backend - fixes (only one email allowed)

--- services/users/create.js
+++ services/users/create.js
@@ -8,15 +8,25 @@ const schema = Joi.object({
   password: Joi.string().required()
 });
 
+const checkEmail = async (email) => {
+  const existingUser = await airDB('users')
+    .select({ filterByFormula: `email="${email}"` })
+    .firstPage();
+
+  if (existingUser && existingUser[0]) {
+    throw new Error('email_taken');
+  }
+};
+
 const create = async (payload) => {
   const { email, fullName, password } = await schema.validateAsync(payload);
 
-  const passwordSalt = crypto.randomBytes(16).toString('hex');
+  await checkEmail(email);
 
+  const passwordSalt = crypto.randomBytes(16).toString('hex');
   const passwordHash = crypto
     .pbkdf2Sync(password, passwordSalt, 1000, 64, 'sha512')
     .toString(`hex`);
-
   const user = await airDB('users').create([
     {
       fields: {
